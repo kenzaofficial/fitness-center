3e4ee7618d05b7afb4532b5a99551d4618381a1b
Юрий Стецкий <yuraha15@yandex.ru>
2022-12-09 22:16:54 +0200

    tabs rdy v.01

--- source/js/main.js
+++ source/js/main.js
@@ -1,52 +1,14 @@
 import { iosVhFix } from './utils/ios-vh-fix';
 import { initModals } from './modules/modals/init-modals';
 import { colorsInit } from './utils/colorsInit';
+import { initTabs } from './utils/init-tabs';
+
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
-  const tabs = document.querySelector('.tabs');
-  const content = document.querySelectorAll('.content');
-  const changeClass = function (element) {
-    for (let i = 0; i < tabs.children.length; i++) {
-      tabs.children[i].classList.remove('tabs__btn--is-active');
-    }
-    element.classList.add('tabs__btn--is-active');
-  };
-  tabs.addEventListener('click', function (event) {
-    const currentTab = event.target.dataset.btn;
-    changeClass(event.target);
-    for (let i = 0; i < content.length; i++) {
-      content[i].classList.remove('tabs__btn--is-active');
-      if (content[i].dataset.content === currentTab) {
-        content[i].classList.add('tabs__btn--is-active');
-      }
-    }
-  });
-
-  // мое дз
-
-  const tabsSecondary = document.querySelector('#tabsSecondary');
-  const contentSecondary = document.querySelectorAll('#contentSecondary');
-  const changeClassSecondary = function (element) {
-    for (let i = 0; i < tabsSecondary.children.length; i++) {
-      tabsSecondary.children[i].classList.remove('tabs__btn--is-active');
-    }
-    element.classList.add('tabs__btn--is-active');
-  };
-  console.log(tabsSecondary);
-
-  tabsSecondary.addEventListener('click', function (event) {
-    const currentTab = event.target.dataset.btnInner;
-    changeClassSecondary(event.target);
-    for (let i = 0; i < contentSecondary.length; i++) {
-      contentSecondary[i].classList.remove('tabs__btn--is-active');
-      if (contentSecondary[i].dataset.contentInner === currentTab) {
-        contentSecondary[i].classList.add('tabs__btn--is-active');
-      }
-    }
-  });
   // colorsInit();
   iosVhFix();
+  initTabs();
 
   // Modules
   // ---------------------------------
